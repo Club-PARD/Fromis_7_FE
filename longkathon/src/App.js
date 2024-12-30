5b2fe15f6cbca51a51bc580c7ac846ad6c4a98cf
Kim woo hyun <151426184+8oo30un@users.noreply.github.com>
2024-12-30 16:19:04 +0900

    [woohyun] - 컬러팔레트 데이터 불러오기

--- longkathon/src/App.js
+++ longkathon/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import LenderPage from './pages/Land.js';
@@ -7,27 +7,11 @@ import MainPage from './pages/MainPage.js';
 import DetailPage from './pages/Detail.js';
 import EmptyMainPage from './pages/EmptyMain.js';
 import AllCategoryPage from './pages/AllCategoryPage.js';
-import ModalAdd from './components/ModalAdd.js';
 import MyInfoPage from './pages/MyInfoPage.js';
 import AddCategory from './pages/AddCategory.js';
 
 function App() {
   return (
-     // const [isModalOpen, setIsModalOpen] = useState(true);
-
-  // const handleSave = (data) => {
-  //     console.log("저장 데이터:", data);
-  //     setIsModalOpen(false);
-  // };
-
-  // const handleClose = () => {
-  //     setIsModalOpen(false);
-  // };
-
-    <>
-      {/* 상단의 AddCategory 컴포넌트 */}
-
-      {/* 라우팅 설정 */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LenderPage />} />
@@ -40,7 +24,6 @@ function App() {
           <Route path="/myinfo" element={<MyInfoPage />} />
         </Routes>
       </BrowserRouter>
-    </>
   );
 }
 
